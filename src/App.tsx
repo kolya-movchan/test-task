cfb5aab9fbb556bc4372a78b57391c92c6838efd
Kolya Movchan <kmovchan261@gmail.com>
2023-05-24 09:37:13 +0300

    Set up environment process

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { Header } from 'components/Header';
-import { Button } from 'components/Button';
-import { ReactComponent as Logo } from 'assets/favicon.svg';
-
 function App() {
-  return (
-    <div className="App">
-      <Header title="hola" />
-      <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>Heyo</Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
